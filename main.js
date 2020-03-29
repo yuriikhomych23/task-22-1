0a70f270cb0f70b00b1e23ed2c3182d16241378b
yuriikhomych23 <yuriikhomych94@gmail.com>
2020-03-29 00:07:59 +0000

    Sample code

--- main.js
+++ main.js
@@ -2,18 +2,18 @@ $(function () {
 
     let widthMax = $(window).width();
     let heightMax = $(window).height();
-    let width = $(window).width();
-    let height = $(window).height();
+    let widthCircle = $(window).width();
+    let heightCircle = $(window).height();
     setInterval(() => {
         $('.circle').animate({
         left: Math.floor( Math.random() * widthMax / 2), 
         top: Math.floor( Math.random() * heightMax / 2),
-        width: Math.floor(Math.random() * width / 2),
-        height: Math.floor(Math.random() * height / 2),
+        width: Math.floor(Math.random() * widthCircle / 2),
+        height: Math.floor(Math.random() * heightCircle / 2),
     }).css({
             backgroundColor: "rgb(" + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + ")",
-            border: "rgb(" + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + ")",
-            boxShadow: Math.floor(Math.random() * 10) + 'px' + ' ' + Math.floor(Math.random() * 10) + 'px' + ' ' + Math.floor(Math.random() * 10) + 'px' + ' ' + "rgb(" + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + ")",
+            borderColor: "rgb(" + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + ")",
+            boxShadow: Math.floor(Math.random() * 20) + 'px' + ' ' + Math.floor(Math.random() * 20) + 'px' + ' ' + Math.floor(Math.random() * 20) + 'px' + ' ' + "rgb(" + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + ")",
     })  
     }, 500);
     
